7e587d7af9af222608d3983a2dfd249feff62a5b
bickyy <vicky.zhou21@sitechhs.com>
2021-01-05 10:18:45 -0500

    imported api

--- basics/src/views/Home.vue
+++ basics/src/views/Home.vue
@@ -1,54 +1,23 @@
 <template>
   <div class="container">
-    <h1>Uh since I can't get the API to work {{ face }}</h1>
-    <button v-on:click="changeFace">Click</button>
-
-    <div id="likes">
-      <button v-on:click="counter +=1">likes</button>
-      <p>{{counter}} likes</p>
-    </div>
-
-    <div id="message"> 
-      <button v-on:click= "popUp">How to make message on top</button>
-    </div>
-
     <div>
-      <h1> make radio</h1>
-      <input type="radio" id="1" value="1" v-model="checkedNames">
-      <label for="1">1</label>
-      <input type="radio" id="2" value="2" v-model="checkedNames">
-      <label for="2">2</label>
-      <input type="radio" id="3" value="3" v-model="checkedNames">
-      <label for="3">3</label>
-      <br>
-      <span> Record answer: {{ checkedNames }}</span>
+           <tr>
+                <th>Country</th>
+                <th>Capital</th>
+                <th>Population</th>
+            </tr>
+            <tr v-for="country in countries_list" :key="country">
+                 <td>
+                    <img v-bind:src="country.flag" alt="Flag" height="26" width="42">
+                    {{country.name}}
+                </td>
+                <td>{{country.capital}}</td>
+                <td>{{country.population}}</td>
+            </tr>
+
+    </div>
   </div>
 
-  <div>
-    <h1> make a menu drop down</h1>
-    <select v-model="dropdown">
-    <option disabled value="">genre</option>
-    <option>comedy</option>
-    <option>horror</option>
-    <option>romance</option>
-    </select>
-    <span>{{ selected }}</span>
-  </div>
-
-  <div>
-    <button @click= 'visible = !visible'> Toggle </button>
-    <transition name = "fade" appear>
-      <div v-if="visible">
-        Hello World
-      </div>
-      <div v-else>
-        Goodbye World
-      </div>
-    </transition>
-  </div>
-
-    
-  </div>
 </template>
 
 <script>
@@ -59,43 +28,32 @@ export default {
   components: {},
   data() {
     return {
-      face: ":)",
-      counter: 0,
-      checkedNames: [],
-      dropdown:{
-        selected: ""
-      },
-      visible: true,
 
+      countries_list:[],
     };
   },
-  methods: {
-    popUp: function () {
-      alert('you made a popup')
-    },
-    changeFace: function() {
-      this.face = ":(";
-    },
-
-  }, 
-  sort(){
-    this.sortedgenders();
+  mounted() {
+    const axios = require('axios');
+        axios
+            .get('https://restcountries.eu/rest/v2/region/americas?fields=name;capital;flag;population')
+            .then(response => (
+                this.countries_list = response.data
+                ))
+            .catch(error =>(
+                console.log(error)
+                ));
   },
+  beforeCreate() {},
 };
-
-
-
 </script>
 
 <style lang="scss" scoped>
-h1 {
-  font-size: 2rem;
-}
-button {
-  font-size: 1.5rem;
-  padding: 1rem;
-  margin: .5rem;
-}
+
+table,
+        th,
+        td {
+            border: 1px solid #cccccc;
+            border-collapse: collapse;
+        }
+
 </style>
-
-
--- basics/src/views/Props.vue
+++ basics/src/views/Props.vue
@@ -1,23 +1,101 @@
 <template>
+  <div class="container">
+    <h1>Uh since I can't get the API to work {{ face }}</h1>
+    <button v-on:click="changeFace">Click</button>
+
+    <div id="likes">
+      <button v-on:click="counter +=1">likes</button>
+      <p>{{counter}} likes</p>
+    </div>
+
+    <div id="message"> 
+      <button v-on:click= "popUp">How to make message on top</button>
+    </div>
+
+    <div>
+      <h1> make radio</h1>
+      <input type="radio" id="1" value="1" v-model="checkedNames">
+      <label for="1">1</label>
+      <input type="radio" id="2" value="2" v-model="checkedNames">
+      <label for="2">2</label>
+      <input type="radio" id="3" value="3" v-model="checkedNames">
+      <label for="3">3</label>
+      <br>
+      <span> Record answer: {{ checkedNames }}</span>
+  </div>
+
   <div>
-    <Child :greeting="greeting" :icecream="icecream" />
+    <h1> make a menu drop down</h1>
+    <select v-model="dropdown">
+    <option disabled value="">genre</option>
+    <option>comedy</option>
+    <option>horror</option>
+    <option>romance</option>
+    </select>
+    <span>{{ selected }}</span>
+  </div>
+
+  <div>
+    <button @click= 'visible = !visible'> Toggle </button>
+    <transition name = "fade" appear>
+      <div v-if="visible">
+        Hello World
+      </div>
+      <div v-else>
+        Goodbye World
+      </div>
+    </transition>
+  </div>
+
+    
   </div>
 </template>
 
 <script>
-import Child from "../components/Child";
+// @ is an alias to /src
 
 export default {
+  name: "Home",
+  components: {},
   data() {
     return {
-      greeting: "Welcome to Props",
-      icecream: ["Chocolate", "Vanilla", "Strawberry", "Pistachio"],
+      face: ":)",
+      counter: 0,
+      checkedNames: [],
+      dropdown:{
+        selected: ""
+      },
+      visible: true,
+
     };
   },
-  components: {
-    Child,
+  methods: {
+    popUp: function () {
+      alert('you made a popup')
+    },
+    changeFace: function() {
+      this.face = ":(";
+    },
+
+  }, 
+  sort(){
+    this.sortedgenders();
   },
 };
+
+
+
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+h1 {
+  font-size: 2rem;
+}
+button {
+  font-size: 1.5rem;
+  padding: 1rem;
+  margin: .5rem;
+}
+</style>
+
+
